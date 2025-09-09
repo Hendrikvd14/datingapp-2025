a834c06ff12da66974703edbe139c50c4d1c60ae
Hendrik Van Dyck <hendrik.vandyck@student.kdg.be>
2025-09-09 19:45:59 +0200

    End of section 8

---
+++ API/Data/Migrations/20250909134018_UpdatedAppUser.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace API.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdatedAppUser : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "MyProperty",
+                table: "Members",
+                newName: "DisplayName");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ImageUrl",
+                table: "Users",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<DateOnly>(
+                name: "DateOfBirth",
+                table: "Members",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: new DateOnly(1, 1, 1));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImageUrl",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "DateOfBirth",
+                table: "Members");
+
+            migrationBuilder.RenameColumn(
+                name: "DisplayName",
+                table: "Members",
+                newName: "MyProperty");
+        }
+    }
+}
